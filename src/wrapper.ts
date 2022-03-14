6ae4557f0826b85cfce732b61c98d21588706987
JF <simplyhiphopmusic@gmail.com>
2022-03-14 11:37:38 -0400

    infer app info

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -4,10 +4,21 @@ let arweaveWallet: ArweaveWebWallet
 
 const run = async () => {
 	const { ArweaveWebWallet } = await import('arweave-wallet-connector')
-	const logo = 'https://arweave.app/arweaveViewLandscape.svg'
-	if (!arweaveWallet) { arweaveWallet = new ArweaveWebWallet({ name: document.title, logo }) }
+	if (!arweaveWallet) { arweaveWallet = new ArweaveWebWallet(getAppInfo()) }
 	arweaveWallet.setUrl('https://arweave.app')
 	arweaveWallet.connect()
+}
+
+const getAppInfo = () => {
+	const hostData = window.location.host.split('.')
+	const name = hostData[hostData.length - 2] || hostData[0]
+	const nodes = document.querySelectorAll("link[rel*='icon']") as NodeListOf<HTMLLinkElement>
+	const sorted = Array.from(nodes).sort((a, b) => +a.sizes?.value?.split('x')?.[0] - +b.sizes?.value?.split('x')?.[0]).reverse()
+	const extracted = sorted[0]?.href
+	let logo: string
+	try { logo ??= new URL(extracted).toString() } catch (e) {}
+	try { logo ??= new URL(window.location.origin + extracted).toString() } catch (e) {}
+	return { name, logo }
 }
 
 window.addEventListener('arweave-app-extension:connect', run)
