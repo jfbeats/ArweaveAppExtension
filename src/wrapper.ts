d9e62da3de4b0e7afcb2fdd1ab5789237715d57e
JF <simplyhiphopmusic@gmail.com>
2022-03-14 20:32:24 -0400

    -

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -13,11 +13,11 @@ const getAppInfo = () => {
 	const hostData = window.location.host.split('.')
 	const name = hostData[hostData.length - 2] || hostData[0]
 	const nodes = document.querySelectorAll("link[rel*='icon']") as NodeListOf<HTMLLinkElement>
-	const sorted = Array.from(nodes).sort((a, b) => +a.sizes?.value?.split('x')?.[0] - +b.sizes?.value?.split('x')?.[0]).reverse()
-	const extracted = sorted[0]?.href
-	let logo: string
-	try { logo ??= new URL(extracted).toString() } catch (e) {}
-	try { logo ??= new URL(window.location.origin + extracted).toString() } catch (e) {}
+	const sorted = Array.from(nodes)
+		.sort((a, b) => +a.sizes?.value?.split('x')?.[0] - +b.sizes?.value?.split('x')?.[0])
+		.sort((a, b) => +(a.rel.includes('apple')) - +(b.rel.includes('apple')))
+		.reverse()
+	const logo = sorted[0]?.href
 	return { name, logo }
 }
 
