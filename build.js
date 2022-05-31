08682625f7722e0f96f73235cd8a0902e06397aa
JF <simplyhiphopmusic@gmail.com>
2022-05-31 15:57:41 -0400

    manifest v2 build wip

--- build.js
+++ build.js
@@ -14,10 +14,17 @@ for (const k in env) { define[`process.env.${k}`] = JSON.stringify(env[k]) }
 
 
 
-const manifest = (config) => {
+const manifest = (target, config) => {
 	const json = fs.readFileSync('manifest.json')
 	const manifest = JSON.parse(json)
-
+	if (target.manifest_version === 2) {
+		manifest.manifest_version = 2
+		manifest.web_accessible_resources = manifest.web_accessible_resources.map(r => r.resources).flat()
+		manifest.browser_action = manifest.action
+		manifest.permissions = manifest.permissions.filter(p => !['scripting'].includes(p))
+		delete manifest.action
+		delete manifest.host_permissions
+	}
 	const result = JSON.stringify(manifest, null, 2)
 	fs.mkdirSync(config.outdir)
 	fs.writeFileSync(config.outdir + '/manifest.json', result)
@@ -25,9 +32,9 @@ const manifest = (config) => {
 
 
 
-export const runBuild = (config) => {
+export const runBuild = (target, config) => {
 	fs.rmSync(config.outdir, { recursive: true, force: true })
-	manifest(config)
+	manifest(target, config)
 	fs.readdirSync('static').forEach(file => fs.copyFileSync('static/' + file, config.outdir + '/' + file))
 	build({ ...config, entryPoints: fs.readdirSync('src').filter(src => src.endsWith('.ts')).map(file => 'src/' + file)})
 	.catch((e) => {
@@ -39,6 +46,8 @@ export const runBuild = (config) => {
 
 
 runBuild({
+	manifest_version: 3,
+}, {
 	minify: true,
 	bundle: true,
 	platform: 'browser',
